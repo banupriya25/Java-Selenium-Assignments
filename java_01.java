dc75233bd0c3e4cae6984a90491b7c8748d93c15
banupriya25 <68202469+banupriya25@users.noreply.github.com>
2020-07-12 16:30:56 -0400

    Add files via upload
    Hello World Pgm

---
+++ java_01.java
@@ -0,0 +1,9 @@
+package java_assign;
+
+public class java_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");	
+	}
+}
---
+++ java_02.java
@@ -0,0 +1,17 @@
+package java_assign;
+
+import java.util.Scanner;
+
+public class java_02{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userinput = new Scanner(System.in);
+		System.out.println("Enter name:");
+		String mystring = userinput.next();
+		System.out.println("Welcome to Java Selenium World " + mystring);
+	
+
+	}
+
+}
